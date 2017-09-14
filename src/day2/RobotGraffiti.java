c0f0b02ff325baaf02ebe518a490d0d0ae30f44e
mjfre <theleague@Matts-MBP.attlocal.net>
2017-09-13 18:35:20 -0700

    More day1 recipes
    Added more day1 recipes
    Added RobotsInSpace and accompanying files
    Added “handouts” for StarShow and FlamingNinjaStar

---
+++ src/day2/RobotGraffiti.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class RobotGraffiti {
+	public static void main(String[] args) {
+		// 1.  Create a new Robot
+		
+		// 2. Make the Robot draw the first letter of your name
+		
+	}
+
+}
