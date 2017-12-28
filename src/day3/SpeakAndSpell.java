8e427ea3dc015fd1b94412948682ec2257aedaec
DaveDLeague <dave.dunn@jointheleague.org>
2017-12-27 16:58:59 -0800

    Update SpeakAndSpell.java

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -5,15 +5,6 @@ import javax.swing.JOptionPane;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
-/**
- * Teacher's Note: 
- * Have the kids play with the Speak & Spell. 
- * The first Speak & Spell was introduced at the summer Consumer Electronics Show in June 1978, making it 
- * one of the earliest handheld electronic devices with a visual display to use interchangeable game cartridges. 
- * Discuss with students how you would make this program. 
- * Allow them to code it themselves, or use this recipe.
- **/
-
 public class SpeakAndSpell {
 
 	public static void main(String[] args) {
